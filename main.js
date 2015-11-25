415db20f71dcfb4560294f44e719c8e4c6e32f66
Yuri Kriachko <yuriykryachko@gmail.com>
2015-11-25 15:57:14 +0200

    show loading

--- main.js
+++ main.js
@@ -8,6 +8,7 @@ var MyBlock = React.createClass({
 
   fetchJoke: function(){
     var _this = this;
+    _this.setState({jokeText: 'Loading..'});
     $.getJSON('http://api.icndb.com/jokes/random', function(data){
 
       var jokeText = data.value.joke;
