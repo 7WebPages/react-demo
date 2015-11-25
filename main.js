bc29c44ce9ee6399a3e1c6bf7dd091c67bcedec2
Yuri Kriachko <yuriykryachko@gmail.com>
2015-11-25 15:26:01 +0200

    implement dummy collapse

--- main.js
+++ main.js
@@ -1,15 +1,40 @@
 var MyBlock = React.createClass({
-  
+
+  getInitialState: function(){
+    return {
+      isExpanded: false
+    }
+  },
+
+  handleClick: function(){
+    this.setState({isExpanded: true});
+  },
+
   render: function(){
-    var myhtml = (<h1>ahah</h1>);
-    return (
-      <h2>Hello, world from component! {this.props.myparam1} {myhtml}</h2>
+    var _this = this;
+    var shortContent =  (
+      <div onClick={this.handleClick}>
+        <h2>Hello, world from component!</h2>
+      </div>
     )
+    var fullContent = (
+      <div>
+        <h2>Hello, world from component!</h2>
+        <p>Some long description</p>
+      </div>
+    )
+
+    if(this.state.isExpanded){
+      return fullContent;
+    }else{
+      return shortContent;
+    }
   }
+
 });
 
 
 ReactDOM.render(
-  <MyBlock myparam1="22" />,
+  <MyBlock />,
   document.getElementById('content')
 );
