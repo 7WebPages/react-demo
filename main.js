c662d11eb22daaa5ae18d0ba6d6096e7fa86b0c5
Yuri Kriachko <yuriykryachko@gmail.com>
2015-11-25 15:38:05 +0200

    erase everything and leave only placeholer

--- main.js
+++ main.js
@@ -2,25 +2,16 @@ var MyBlock = React.createClass({
 
   getInitialState: function(){
     return {
-      isExpanded: false
     }
   },
 
-  handleClick: function(){
-    this.setState({isExpanded: !this.state.isExpanded});
-  },
+
 
   render: function(){
 
-    var extraContent = null;
-    if(this.state.isExpanded){
-      extraContent = (<p>Some long description</p>);
-    }
-
     return (
-      <div onClick={this.handleClick}>
-        <h2>Hello, world from component!</h2>
-        {extraContent}
+      <div >
+        <h2>I'm a placeholder</h2>
       </div>
     )
 
